816daec8d333c3f0563d1503a3eef1b738319ea6
Satoru <satoru_kishi@hotmail.com>
2018-07-25 00:25:20 -0300

    XF.AplicativoFIAP

---
+++ XF.AplicativoFIAP/XF.AplicativoFIAP/View/NovoProfessorView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.AplicativoFIAP.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NovoProfessorView : ContentPage
+	{
+		public NovoProfessorView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ XF.AplicativoFIAP/XF.AplicativoFIAP/View/ProfessorView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.AplicativoFIAP.ViewModel;
+
+namespace XF.AplicativoFIAP.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProfessorView : ContentPage
+	{
+		public ProfessorView ()
+		{
+            BindingContext = ProfessorViewModel.Instancia;
+			InitializeComponent ();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            lstProfessores.IsRefreshing = !lstProfessores.IsRefreshing;
+            await ProfessorViewModel.Instancia.Carregar();
+            lstProfessores.IsRefreshing = !lstProfessores.IsRefreshing;
+        }
+    }
+}
